4f10e3da4b8fd2415a46669a8599baab34883dab
zitounirania <mr_zitouni@esi.dz>
2024-10-14 00:45:32 +0100

    setup the app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline font-atheletic text-etic-red">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
